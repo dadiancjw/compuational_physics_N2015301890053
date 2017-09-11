4335b9e1c4535e5840746da38280d3d433f66006
dadiancjw <31840717+dadiancjw@users.noreply.github.com>
2017-09-11 20:09:13 +0800

    Add files via upload

---
+++ exercise01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 11 18:57:58 2017
+
+@author: 陇溪首逸
+"""
+
+print("     O    O            0               0     \n",
+      " OOOOOOOOOOOO        O                O O   \n",
+      "    O    O           O  O            O  O   \n",
+      "                    O   O           O   O   \n",
+      "   OOO OOOO        O    O          O    O   \n",
+      "   O O O  O       OO  OOOOO       OO    O   \n",
+      "  OO O  O O      O O    O        O O  OOOOO \n",
+      " OO O    OO     O  O OOOOOOO    O  O    O   \n",
+      "O  O OOO  O        O    O          O  OOOOO \n",
+      "  O   O    OO      O    O          O    O   \n",
+      " O OOOOOOO   O     O  OOOOO        O OOOOOOO\n",
+      "     OOO           O    O          O    O  O\n",
+      "    O O O          O    O          O    O  O\n",
+      "      O  O         O OOOOOOO       O    O OO\n",
+      "     OO            O               O    O   \n")
