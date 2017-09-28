3aabc78445bdf7069dbd8bdae250d48582951942
dadiancjw <31840717+dadiancjw@users.noreply.github.com>
2017-09-28 21:44:03 +0800

    Update my_walking_name.py

--- my_walking_name.py
+++ my_walking_name.py
@@ -1,5 +1,6 @@
+# 名字的首字母简称 CJW 在同一输出行移动
 import time
 for j in range(0,10):
-   for i in range(0,20):
-       print("    "*i,"CJW", end='\r')
-       time.sleep(0.5)
+   for i in range(0,50):
+       print("  "*i,"CJW", end='\r')
+       time.sleep(0.14)
