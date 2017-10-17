74062533564b25cd356054ca8edea928a7552a33
dadiancjw <31840717+dadiancjw@users.noreply.github.com>
2017-10-17 14:18:39 +0800

    Update exercise05.py

--- exercise05.py
+++ exercise05.py
@@ -5,7 +5,8 @@ v0=40
 xi=x0=0
 yi=y0=0
 zi=z0=0
-omg=20
+omg=3
+theta=0
 g=9.8
 vxi=v0*(math.cos(20*math.pi/180))
 vyi=v0*(math.sin(20*math.pi/180))
@@ -17,9 +18,10 @@ t=3
 dt=t/10000
 while i<=10000:
     v=math.sqrt(vxi**2+vyi**2)
-    vxi=vxi-0.0039*v*vxi*dt-0.0058/(1+math.exp((v-35)/5))*v*vxi*dt
+    theta=theta+omg*dt
+    vxi=vxi-0.0058/(1+math.exp((v-35)/5))*v*vxi*dt
     vyi=vyi-g*dt
-    vzi=vzi-vxi*omg*0.00041*dt
+    vzi=vzi-vxi*omg*0.00041*dt-4.9*(math.sin(4*theta)-0.25*math.sin(8*theta)+0.08*math.sin(12*theta)-0.025*math.sin(16*theta))*dt
     xi=xi+vxi*dt
     yi=yi+vyi*dt
     zi=zi+vzi*dt
@@ -28,11 +30,10 @@ while i<=10000:
     zlist.append(zi)
     i=i+1
 pl.figure(figsize=(20, 7))
-p1,=pl.plot(xlist,ylist,'k')
 p2,=pl.plot(xlist,zlist,'b')
-pz,=pl.plot([0,80],[0,0],'r')
+pz,=pl.plot([0,93],[0,0],'r')
 pl.title('Batted Ball Trajectory',fontsize=40)
 pl.xlabel('X Distance',fontsize=26)
-pl.ylabel('Y/Z',fontsize=30)
-pl.legend([pz,p1,p2],['Horizon Line','X','Z'],fontsize=24,loc="upper left")
+pl.ylabel('Z',fontsize=30)
+pl.legend([pz,p2],['Straight Line','Z'],fontsize=24,loc="lower left")
 pl.show()
