9fb2feead6a69e9d58097d2f9e3dfb7b3afaa6ba
dadiancjw <31840717+dadiancjw@users.noreply.github.com>
2017-10-25 22:55:57 +0800

    Update pendulum.py

--- pendulum.py
+++ pendulum.py
@@ -3,7 +3,7 @@ import pylab as pl
 g=9.8
 l=9.8
 q=0.5
-F=0.5
+F=0
 wd=2/3
 dt=0.04
 w0=0
@@ -16,21 +16,23 @@ wlist=[wi]
 rlist=[ri]
 tlist=[ti]
 i=1
-while i<200:
-    wi=wi-g/l*math.sin(ri)*dt+q*wi*dt-F*math.sin(wd*ti)*dt
+while i<700:
+    wi=wi-g/l*math.sin(ri)*dt-q*wi*dt+F*math.sin(wd*ti)*dt
     wlist.append(wi)
     ri=ri+wi*dt
-    rlist.append(ri)
-    ti=ti+dt
     while ri>math.pi:
         ri=ri-2*math.pi
     while ri<-math.pi:
         ri=ri+2*math.pi
+    rlist.append(ri)
+    ti=ti+dt
     tlist.append(ti)
     i=i+1
-pl.figure(figsize=(10, 8))
-pl.plot(tlist,rlist)
-pl.title('Chaos Systerm',fontsize=30)
-pl.xlabel('time/s',fontsize=20)
-pl.ylabel('theta/radians',fontsize=20) #pl.legend([pz,p1,p2],['Straight Line','omg=10','omg=3'],fontsize=34,loc="lower left")
+pl.figure(figsize=(20, 14))
+p1,=pl.plot(tlist,rlist,'k')
+p2,=pl.plot(tlist,wlist,'r')
+pl.title('Chaos Systerm',fontsize=50)
+pl.xlabel('time/s',fontsize=30)
+pl.ylabel('theta/radians',fontsize=30)
+pl.legend([p1,p2],['Theta','Omega'],fontsize=40,loc="upper right")
 pl.show()
