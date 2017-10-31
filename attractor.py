8735288cece2dc179f3607f1d108d62f9e22433e
dadiancjw <31840717+dadiancjw@users.noreply.github.com>
2017-10-31 09:27:12 +0800

    Update attractor.py

--- attractor.py
+++ attractor.py
@@ -12,29 +12,29 @@ t0=0
 wi=w0
 ri=r0
 ti=t0
-wlist=[]
-rlist=[]
+wlist=[wi]
+rlist=[ri]
 tlist=[ti]
 i=1
-while i<10000:
-    wi=wi-g/l*math.sin(ri)*dt-q*wi*dt+F*math.sin(wd*ti)*dt
+while i<100000:
+    a=wd*ti
+    wi=wi-g/l*math.sin(ri)*dt-q*wi*dt+F*math.sin(a)*dt
     ri=ri+wi*dt
     while ri>math.pi:
         ri=ri-2*math.pi
     while ri<-math.pi:
         ri=ri+2*math.pi
-    a=wd*ti
     while a>math.pi:
         a=a-math.pi
-    while abs(a-math.pi)<0.02:
+    if (math.pi-a)<0.02:
         wlist.append(wi)
         rlist.append(ri)
     ti=ti+dt
     tlist.append(ti)
     i=i+1
 pl.figure(figsize=(20, 14))
-pl.plot(rlist,wlist,'r')
+pl.plot(rlist,wlist,'ko')
 pl.title('Chaos Systerm',fontsize=50)
-pl.xlabel('radians',fontsize=30)
+pl.xlabel('theta/radians',fontsize=30)
 pl.ylabel('omega',fontsize=30)
 pl.show()
